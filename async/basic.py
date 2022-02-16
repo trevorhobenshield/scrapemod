cf5154b11a5b4153a0c83c1ed2ef4d870e828a6f
trevorhobenshield <trevorhobenshield@gmail.com>
2022-02-16 13:52:52 -0800

    update

--- async/basic.py
+++ async/basic.py
@@ -25,16 +25,17 @@ async def get(url: str, session: aiohttp.ClientSession) -> Any:
         print(e)
 
 
-async def process_requests(urls: list) -> list:
-    async with aiohttp.ClientSession(headers=get_headers('headers.txt')) as s:
+async def process_requests(urls: list, headers: dict) -> list:
+    async with aiohttp.ClientSession(headers=headers) as s:
         return await asyncio.gather(*(get(u, s) for u in urls))
 
 
 def main():
     set_logger('myLog.log')
     urls = Path('urls.txt').read_text().splitlines()
+    headers = get_headers('headers.txt')
     loop = asyncio.get_event_loop()
-    res = loop.run_until_complete(process_requests(urls))
+    res = loop.run_until_complete(process_requests(urls, headers))
     save_html(res)
 
 
