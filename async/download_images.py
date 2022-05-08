ad9a2b2001194843e96600a90562b3f2895d5117
y <trevorhobenshield@gmail.com>
2022-05-08 14:55:36 -0700

    update

--- async/download_images.py
+++ async/download_images.py
@@ -16,22 +16,22 @@ asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())  # modified event loop
 async def download_image(url: str, session: aiohttp.ClientSession):
     try:
         logging.debug(f"Downloading: {url}")
-        response = await session.request(method='GET', url=url)
-        content = await response.read()
+        res = await session.request(method='GET', url=url)
+        dat = await res.read()
         unique_filename = hashlib.md5(url.encode("utf-8")).hexdigest()
         async with aiofiles.open(f'{unique_filename}.png', 'wb') as fw:
-            await fw.write(content)
+            await fw.write(dat)
     except Exception as e:
         print(e)
 
 
-async def process_requests(urls: list, headers: dict):
+async def process_requests(urls: list[str], headers: dict):
     async with aiohttp.ClientSession(headers=headers) as s:
         await asyncio.gather(*(download_image(u, s) for u in urls))
 
 
 def main():
-    set_logger('myLog.log')
+    set_logger('downloaded_images.log')
     urls: list[str] = Path('urls.txt').read_text().splitlines()
     loop = asyncio.get_event_loop()
     loop.run_until_complete(process_requests(urls, get_headers('headers.txt')))
